f2dbd66899973ec2b8cabe96e2687be3a2b5fde5
Darshan L <darshanl1254@gmail.com>
2024-12-13 22:43:38 +0530

    Recharge Front page done

--- src/main/java/org/dars/recharge_task/controller/RechargeController.java
+++ src/main/java/org/dars/recharge_task/controller/RechargeController.java
@@ -1,5 +1,13 @@
 package org.dars.recharge_task.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RechargeController {
 
+	@GetMapping("/")
+	public String page() {
+		return "recharge.html";
+	}
 }
