4165fb39c363476fc7faeb1e1fbe212d84bf08cf
Darshan L <darshanl1254@gmail.com>
2024-12-17 00:15:04 +0530

    Email Done

--- src/main/java/org/dars/recharge_task/controller/RechargeController.java
+++ src/main/java/org/dars/recharge_task/controller/RechargeController.java
@@ -3,18 +3,19 @@ package org.dars.recharge_task.controller;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.thymeleaf.TemplateEngine;
 
 import com.razorpay.Order;
 import com.razorpay.RazorpayClient;
 import com.razorpay.RazorpayException;
 
+import jakarta.mail.internet.MimeMessage;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -25,13 +26,13 @@ public class RechargeController {
 	@Value("${razorpay.secret}")
 	private String secret;
 
+	@Autowired
+	JavaMailSender mailSender;
+
 	@GetMapping("/")
 	public String page() {
 		return "recharge.html";
 	}
-
-	@Autowired
-	TemplateEngine engine;
 
 	@PostMapping("/recharge")
 	public String recharge(@RequestParam long mobile, @RequestParam String email, @RequestParam String sim,
@@ -60,9 +61,26 @@ public class RechargeController {
 	}
 
 	@PostMapping("/success")
-	@ResponseBody
-	public String success() {
-		return "<h2 style=\"color:green; text-align:center;\">Recharge Payment Success</h2>";
+	public String success(HttpSession session, @RequestParam String razorpay_payment_id, ModelMap map) {
+		sendMail((String) session.getAttribute("email"), (double) session.getAttribute("amount"), razorpay_payment_id);
+		map.put("success", "Recharge Done Successfully");
+		return "recharge.html";
+	}
+
+	void sendMail(String email, double amount, String paymentId) {
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+
+		try {
+			helper.setFrom("darshanl1254@gmail.com", "Recharge App");
+			helper.setTo(email);
+			helper.setSubject("Recharge Confirmation!!!");
+			helper.setText("<h1 style=\"color:green; text-align:center;\">The Recharge of " + amount
+					+ " was Successfull<br>Thank You...</h1>", true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		mailSender.send(message);
 	}
 
 }
